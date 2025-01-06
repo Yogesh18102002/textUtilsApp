94d77888da0261274d556ad06861e47e3b34637a
Yogesh <yogeshiswalkar18@gmail.com>
2025-01-06 18:57:03 +0530

    Have a look at module1.mjs and module2.mjs. Which showcase concept of Export defaults.

---
+++ module1.mjs
@@ -0,0 +1,4 @@
+import abc, {a} from './module2.mjs';
+
+console.log(abc);
+console.log(a);
---
+++ module2.mjs
@@ -0,0 +1,7 @@
+const a = "Rakesh";
+const b = "Kumar";
+const c = "Singh";
+const d = "Default Text";
+
+export default d;
+export { a, b, c };
